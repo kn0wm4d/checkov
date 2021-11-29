e29a8c107753e54037a198b9ad1d20c78e8d9744
ALVAREZ DELGADO Xavier <xavier.alvarez@axa-assistance.es>
2021-11-29 15:43:31 +0100

    Add terraform check

---
+++ checkov/terraform/checks/resource/azure/AXA_StorageAccountITPMTag.py
@@ -0,0 +1,24 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class AXA_StorageAccountITPMTag(BaseResourceCheck):
+    def __init__(self):
+        description = "Ensure that resources are created with the ITPM tag"
+        id = "AXA_AZURE_133"
+        supported_resources = ['azurerm_storage_account']
+
+        # Valid CheckCategories are defined in checkov/common/models/enums.py
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=description, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        if 'tags' in conf.keys():
+            if "local-itpm" in conf["tags"].keys():
+                if len(conf["tags"]["local-itpm"]) > 0:
+                    return CheckResult.PASSED
+
+        return CheckResult.FAILED
+
+
+check = AXA_StorageAccountITPMTag()
